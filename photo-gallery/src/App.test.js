de2386cbab03ecd73235a3976a932330a5c8cd6a
Owen Prendergast <prendergasto@gmail.com>
2020-02-12 14:22:53 -0800

    did some tests

--- photo-gallery/src/App.test.js
+++ photo-gallery/src/App.test.js
@@ -8,3 +8,28 @@ test('renders App.js correctly', () => {
   .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+import React from 'react';
+// import { render } from '@testing-library/react';
+import renderer from "react-test-renderer";
+import App from './App';
+import Header from './header';
+test('renders App.js correctly', () => {
+  const tree = renderer
+  .create(<Header />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+import React from 'react';
+// import { render } from '@testing-library/react';
+import renderer from "react-test-renderer";
+import App from './App';
+import Header from './header';
+import ImageItem from './imageItem';
+test('renders App.js correctly', () => {
+  const tree = renderer
+  .create(<ImageItem />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
